8c1137a21fd1155dce2f7a02270a6eb8eea40582
Bartek1887 <54062598+bartek18887@users.noreply.github.com>
2022-12-03 20:06:31 +0100

    owo

--- launcher/ui/pages/global/AccountListPage.cpp
+++ launcher/ui/pages/global/AccountListPage.cpp
@@ -188,7 +188,7 @@ void AccountListPage::on_actionAddMicrosoft_triggered()
 
 void AccountListPage::on_actionAddOffline_triggered()
 {
-    if (!m_accounts->anyAccountIsValid()) {
+    if (false) {
         QMessageBox::warning(
             this,
             tr("Error"),
